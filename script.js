338677a134d39330eaac553eacbc512ad404f8b4
Abhay Khawas <abhaykhawas@gmail.com>
2021-10-05 00:58:06 +0530

    Have reading writing property but not yet finished

--- script.js
+++ script.js
@@ -1,30 +1,84 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBEucPuUtb_vbR8RJRla7-kjVvcWT4Scvw",
+    authDomain: "testing-d0664.firebaseapp.com",
+    databaseURL: "https://testing-d0664-default-rtdb.firebaseio.com",
+    projectId: "testing-d0664",
+    storageBucket: "testing-d0664.appspot.com",
+    messagingSenderId: "358791211092",
+    appId: "1:358791211092:web:d1e1cd1444a59e5d3225c1"
+  };
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+onload = function() {
+
+    const tasks = firebase.database().ref('Todo');
+    tasks.on('value', (snap) => {
+        const todos = snap.val();
+        // console.log(todos)
+        const taskList = [];
+        for (let id in todos) {
+          taskList.push({ id, ...todos[id] });
+ 
+        }
+        for(var i=0; i<taskList.length;i++){
+            var li = document.createElement("li");
+            li.appendChild(document.createTextNode(taskList[i].todo));
+            ul.appendChild(li);
+            // console.log(taskList[i].todo)
+            // console.log(taskList[i].id)
+        }
+        document.querySelectorAll("li").forEach(element => {
+            element.addEventListener('click', ()=>{
+                for (let id in todos) {
+                    console.log({ id, ...todos[id] })
+                }
+                // console.log(element.innerHTML)
+            })
+        });
+      });
+};
+
 var ul = document.getElementById("list");
 var input = document.getElementById("input")
 var submit = document.getElementById("submit")
 var lis = document.querySelectorAll("li")
 
 submit.addEventListener("click", () =>{
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
-    input.value = '';
+    if(input.value != ''){
+        var ul = document.querySelector('ul');
+        ul.innerHTML = '';
+        // var li = document.createElement("li");
+        // li.appendChild(document.createTextNode(input.value));
+        // console.log(li)
+        // ul.appendChild(li);
+        // console.log(ul)
+        b = input.value ;
+        let todoRef = firebase.database().ref('Todo');
+        // console.log(todoRef)
+        let a = todoRef.push();
+        a.set({
+            todo:b
+        })
+        alert("Done")
+        input.value = '';
+    }
+
     lis = document.querySelectorAll("li")
-    console.log(lis)
+    // console.log(lis)
+    document.querySelectorAll("li").forEach(element => {
+        element.addEventListener('click', ()=>{
+            element.remove()
+            console.log(element.innerHTML)
+        })
+    });
 })
 
 
-// for(var i =0; i<lis.length; i++){
-//  lis = document.querySelectorAll("li")
-//  lis[i].addEventListener('click',()=>{
-//     // ul.removeChild(ul.childNodes[i])
-//     // lis[0].remove()
-//     console.log(i)
-
-//  })   
-// }
-
-document.querySelectorAll("li").forEach(element => {
+document.querySelectorAll(".list li").forEach(element => {
     element.addEventListener('click', ()=>{
+        element.remove()
         console.log(element.innerHTML)
     })
 });
